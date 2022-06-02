9ca16e1115d07b653c26a16029c47b4108a2b925
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-02 15:03:58 -0700

    Add account_summary view and relationships

---
+++ optum-edb-test-1/hasura/migrations/default/1654207357548_create_account_summary_view/down.sql
@@ -0,0 +1,11 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create or replace view account_summary as
+-- select
+--   account.id,
+--   sum(units*price)
+--   from account
+--        join "order" on "order".account_id = account.id
+--        join order_detail on order_detail.order_id = "order".id
+--        join product on product.id = order_detail.product_id
+--  group by account.id;
