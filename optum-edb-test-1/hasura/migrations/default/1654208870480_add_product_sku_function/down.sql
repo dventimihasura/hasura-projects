81f2560400d81b58474d060230802d6248bf1433
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-02 15:30:04 -0700

    Add product_sku function and track it as a computed field

---
+++ optum-edb-test-1/hasura/migrations/default/1654208870480_add_product_sku_function/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create or replace function product_sku(product_row product)
+-- returns text as $$
+--   select md5(product_row.name)
+-- $$ language sql stable;
