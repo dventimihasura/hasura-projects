abe1bd0c83c1f533a6ab8e598f848f0445b16e39
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-02 15:21:28 -0700

    Add search functions

---
+++ optum-edb-test-1/hasura/migrations/default/1654208382805_add_search_functions/down.sql
@@ -0,0 +1,25 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create or replace function product_search(search text)
+--   returns setof product as $$
+--   select *
+--   from product
+--   where
+--   name ilike ('%' || search || '%')
+-- $$ language sql stable;
+--
+-- create extension if not exists pg_trgm;
+--
+-- create index if not exists product_gin_idx on product
+-- using gin ((name) gin_trgm_ops);
+--
+-- create or replace function product_fuzzy_search(search text)
+--   returns setof product as $$
+--   select *
+--   from product
+--   where
+--   search <% (name)
+--   order by
+--   similarity(search, (name)) desc
+--   limit 5;
+-- $$ language sql stable;
