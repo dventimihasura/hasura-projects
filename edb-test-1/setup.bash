d6c844f9c4586ebe2135fa50239f9488874f3776
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-03 16:23:31 -0700

    Added helpful comments to the setup.bash script output

--- edb-test-1/setup.bash
+++ edb-test-1/setup.bash
@@ -1,19 +1,13 @@
 # Generate an environment file for Docker Compose
 cat <<EOF > .env
-PGHOST=localhost
-PGPORT=6432
-PGDATABASE=postgres
-PGUSER=postgres
-PGPASSWORD=postgrespassword
-HASURAPORT=6432
-HASURA_GRAPHQL_METADATA_DATABASE_URL=postgres://postgres:postgrespassword@postgres:5432/postgres
-PG_DATABASE_URL=postgres://postgres:postgrespassword@postgres:5432/postgres
-HASURA_GRAPHQL_ENABLE_CONSOLE="true"
-HASURA_GRAPHQL_DEV_MODE="true"
-HASURA_GRAPHQL_ENABLED_LOG_TYPES=startup, http-log, webhook-log, websocket-log, query-log
+PGHOST=localhost		# CHANGE ME
+PGPORT=6432			# CHANGE ME
+PGDATABASE=postgres		# CHANGE ME
+PGUSER=postgres			# CHANGE ME
+PGPASSWORD=postgrespassword	# CHANGE ME
 EOF
 
 # Generate a environment file for Hasura CLI
 cat <<EOF > hasura/.env
-HASURA_GRAPHQL_ENDPOINT=http://localhost:8080
+HASURA_GRAPHQL_ENDPOINT=http://localhost:8080 # CHANGE ME
 EOF
