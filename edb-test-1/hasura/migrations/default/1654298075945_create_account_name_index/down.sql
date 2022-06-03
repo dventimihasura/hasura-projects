15b4f82047dbf5239f23b03946ff2a03619f974b
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-03 16:20:47 -0700

    Added an index on account(name) to speed up text queries

---
+++ edb-test-1/hasura/migrations/default/1654298075945_create_account_name_index/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create index if not exists account_name_idx on account (name);
---
+++ edb-test-1/hasura/migrations/default/1654298075945_create_account_name_index/up.sql
@@ -0,0 +1,1 @@
+create index if not exists account_name_idx on account (name);
