6e2d93dbe7518183d8338d006387e3f78cd830e0
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-06-22 17:01:00 -0700

    Generated orders

---
+++ postgres-authorization-1/hasura/migrations/default/1655942437553_generate_invoices/down.sql
@@ -0,0 +1,42 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- begin;
+-- with
+--   account as (
+--     select
+--       account.id,
+--       name,
+--       (random()*5)::int orders
+--       from account)
+-- insert into invoice (account_id)
+-- select
+--   account_id
+--     from (
+--       select
+-- 	account.id account_id,
+-- 	row_number() over (partition by account.id order by account.name) ordinal
+-- 	from account, generate_series(1, 5)) orders
+-- 	 join account on account.id = orders.account_id
+-- 		and orders.ordinal <= account.orders;
+--
+--
+-- with
+--   invoice as (
+--     select
+--       invoice.id,
+--       (random()*9 + 1)::int items
+--       from invoice)
+-- insert into line_item (invoice_id, product_id, units)
+-- select
+--   invoice_id,
+--   product_id,
+--   (random()*9 + 1)::int units
+--     from (
+--       select
+-- 	invoice.id invoice_id,
+-- 	product.id product_id,
+-- 	row_number() over (partition by invoice.id) ordinal
+-- 	from invoice, product) user_item
+-- 	 join invoice on invoice.id = user_item.invoice_id
+-- 	     and user_item.ordinal <= invoice.items;
+-- commit;
