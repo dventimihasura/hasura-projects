13f8f8f33a889acb2582a829efdb2a7fe4ed4547
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-06 15:52:17 -0700

    multi-role-1: Added a setup script

---
+++ multi-role-1/setup.bash
@@ -0,0 +1,15 @@
+cat <<EOF > hasura/local1.env
+HASURA_GRAPHQL_ENDPOINT=http://localhost:8081
+EOF
+
+cat <<EOF > hasura/local2.env
+HASURA_GRAPHQL_ENDPOINT=http://localhost:8082
+EOF
+
+cat <<EOF > hasura/local3.env
+HASURA_GRAPHQL_ENDPOINT=http://localhost:8083
+EOF
+
+cat <<EOF > hasura/local4.env
+HASURA_GRAPHQL_ENDPOINT=http://localhost:8084
+EOF
