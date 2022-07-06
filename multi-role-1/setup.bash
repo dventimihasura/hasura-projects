3cbf30cfe3d694e4f3e68bf9932bda869aee9986
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-06 16:07:14 -0700

    multi-role-1: Added the admin secret to the setup scripts

--- multi-role-1/setup.bash
+++ multi-role-1/setup.bash
@@ -1,15 +1,19 @@
 cat <<EOF > hasura/local1.env
 HASURA_GRAPHQL_ENDPOINT=http://localhost:8081
+HASURA_GRAPHQL_ADMIN_SECRET=myadminsecretkey
 EOF
 
 cat <<EOF > hasura/local2.env
 HASURA_GRAPHQL_ENDPOINT=http://localhost:8082
+HASURA_GRAPHQL_ADMIN_SECRET=myadminsecretkey
 EOF
 
 cat <<EOF > hasura/local3.env
 HASURA_GRAPHQL_ENDPOINT=http://localhost:8083
+HASURA_GRAPHQL_ADMIN_SECRET=myadminsecretkey
 EOF
 
 cat <<EOF > hasura/local4.env
 HASURA_GRAPHQL_ENDPOINT=http://localhost:8084
+HASURA_GRAPHQL_ADMIN_SECRET=myadminsecretkey
 EOF
