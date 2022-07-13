d200f6f3efe6f359e31e8db7b576b34ad47d6f67
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-13 07:02:11 -0700

    Killed the events

--- edb-test-1/hasura/migrations/default/1657644362438_alter_table_public_misc_add_column_id/down.sql
+++ edb-test-1/hasura/migrations/default/1657644362438_alter_table_public_misc_add_column_id/down.sql
@@ -2,3 +2,6 @@
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."misc" add column "id" serial
 --  not null unique;
+
+alter table "public"."misc" drop column "id";
+
