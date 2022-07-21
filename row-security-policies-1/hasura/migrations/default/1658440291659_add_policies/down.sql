533618042f5061a29c8b22733964193689b99d7d
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-21 14:52:28 -0700

    Added policies

---
+++ row-security-policies-1/hasura/migrations/default/1658440291659_add_policies/down.sql
@@ -0,0 +1,15 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- -*- sql-product: postgres; -*-
+--
+-- create policy query on account for select using (true);
+--
+-- create policy mutation on account for all using ((current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid = id);
+--
+-- create policy query on "order" for select using (true);
+--
+-- create policy mutation on "order" for all using ((current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid = account_id);
+--
+-- create policy query on order_detail for select using (true);
+--
+-- create policy mutation on order_detail for all using (exists (select 1 from "order" where id = order_id and (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid = account_id));
