9b593310213c9b02e64d226298e3fa7e0d41f73a
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-21 15:01:42 -0700

    Enabled RLS

---
+++ row-security-policies-1/hasura/migrations/default/1658440730775_enable_rls/down.sql
@@ -0,0 +1,13 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table account enable row level security;
+--
+-- alter table account force row level security;
+--
+-- alter table "order" enable row level security;
+--
+-- alter table "order" force row level security;
+--
+-- alter table order_detail enable row level security;
+--
+-- alter table order_detail force row level security;
---
+++ row-security-policies-1/hasura/migrations/default/1658440730775_enable_rls/up.sql
@@ -0,0 +1,11 @@
+alter table account enable row level security;
+
+alter table account force row level security;
+
+alter table "order" enable row level security;
+
+alter table "order" force row level security;
+
+alter table order_detail enable row level security;
+
+alter table order_detail force row level security;
