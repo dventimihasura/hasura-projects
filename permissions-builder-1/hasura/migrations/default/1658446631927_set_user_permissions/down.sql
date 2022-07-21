f731b7f1a331f37d7129d66c4b84b1d0826332b4
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-21 16:39:20 -0700

    Set user permissions

---
+++ permissions-builder-1/hasura/migrations/default/1658446631927_set_user_permissions/down.sql
@@ -0,0 +1,20 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- with
+--   "user" as (
+--     select
+--       "user".id,
+--       (random()*9 + 1)::int items
+--       from "user")
+--     insert into user_permission (user_id, permission_id)
+-- select
+--   user_id,
+--   permission_id
+--   from (
+--     select
+--       "user".id user_id,
+--       permission.id permission_id,
+--       row_number() over (partition by "user".id) ordinal
+--       from "user", permission) user_item
+--        join "user" on "user".id = user_item.user_id
+-- 	   and user_item.ordinal <= "user".items;
