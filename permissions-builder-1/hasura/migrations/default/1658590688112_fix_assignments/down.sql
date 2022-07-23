456a3b436c9f8f8ca4f2b58dbc6ce29d25de1fa5
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-23 08:42:00 -0700

    Fixed the assignments
    users are only assigned to projects at their own organization

---
+++ permissions-builder-1/hasura/migrations/default/1658590688112_fix_assignments/down.sql
@@ -0,0 +1,28 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- delete from assignment;
+--
+-- with
+--   "user" as (
+--     select
+--       "user".id,
+--       "user".organization_id,
+--       (random()*9 + 1)::int items
+--       from "user")
+--     insert into assignment (user_id, project_id)
+-- select
+--   user_id,
+--   project_id
+--   from (
+--     select
+--       "user".id user_id,
+--       project.id project_id,
+--       row_number() over (partition by "user".id) ordinal
+--       from
+-- 	"user",
+-- 	project join organization on organization_id = organization.id
+--      where
+--        "user".organization_id = organization.id
+--   ) user_item
+--        join "user" on "user".id = user_item.user_id
+-- 	   and user_item.ordinal <= "user".items;
