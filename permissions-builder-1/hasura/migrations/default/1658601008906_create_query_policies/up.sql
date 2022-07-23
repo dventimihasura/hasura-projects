3409b9614b5559af798f9982a0b402932f8c54a2
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-23 11:30:43 -0700

    Adding query policies

---
+++ permissions-builder-1/hasura/migrations/default/1658601008906_create_query_policies/up.sql
@@ -0,0 +1,13 @@
+create policy query on assignment for select using (true);
+
+create policy query on organization for select using (true);
+
+create policy query on permission for select using (true);
+
+create policy query on project for select using (true);
+
+create policy query on timesheet for select using (true);
+
+create policy query on "user" for select using (true);
+
+create policy query on user_permission for select using (true);
