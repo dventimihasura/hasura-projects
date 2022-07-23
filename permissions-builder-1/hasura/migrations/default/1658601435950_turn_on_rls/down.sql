5611f0a7b926cd24f70c59a76cd9128a3b6a9f3b
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-23 11:37:43 -0700

    Turn on RLS

---
+++ permissions-builder-1/hasura/migrations/default/1658601435950_turn_on_rls/down.sql
@@ -0,0 +1,29 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table assignment enable row level security;
+--
+-- alter table assignment force row level security;
+--
+-- alter table organization enable row level security;
+--
+-- alter table organization force row level security;
+--
+-- alter table permission enable row level security;
+--
+-- alter table permission force row level security;
+--
+-- alter table project enable row level security;
+--
+-- alter table project force row level security;
+--
+-- alter table timesheet enable row level security;
+--
+-- alter table timesheet force row level security;
+--
+-- alter table "user" enable row level security;
+--
+-- alter table "user" force row level security;
+--
+-- alter table user_permission enable row level security;
+--
+-- alter table user_permission force row level security;
---
+++ permissions-builder-1/hasura/migrations/default/1658601435950_turn_on_rls/up.sql
@@ -0,0 +1,27 @@
+alter table assignment enable row level security;
+
+alter table assignment force row level security;
+
+alter table organization enable row level security;
+
+alter table organization force row level security;
+
+alter table permission enable row level security;
+
+alter table permission force row level security;
+
+alter table project enable row level security;
+
+alter table project force row level security;
+
+alter table timesheet enable row level security;
+
+alter table timesheet force row level security;
+
+alter table "user" enable row level security;
+
+alter table "user" force row level security;
+
+alter table user_permission enable row level security;
+
+alter table user_permission force row level security;
