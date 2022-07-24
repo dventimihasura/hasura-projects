365aaf0168aa8ebd0984d747c6b3e8ae9d34544a
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-24 05:12:39 -0700

    Set up RLS

--- permissions-builder-1/docker-entrypoint-initdb.d/00_init.sql
+++ permissions-builder-1/docker-entrypoint-initdb.d/00_init.sql
@@ -21,11 +21,47 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 ------------------------------------------------------------------------------
 
 -- create a separate user for to manage user database
-CREATE USER hasurauser WITH PASSWORD 'hasurauser';
+CREATE USER hasuramanager WITH PASSWORD 'hasuramanager';
 
 -- create pgcrypto extension, required for UUID
 -- See section below on pgcrypto in PG search path
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- The below permissions are optional. This is dependent on what access to your
+-- tables/schemas you want give to hasura. If you want expose the public
+-- schema for GraphQL query then give permissions on public schema to the
+-- hasura user.
+-- Be careful to use these in your production db. Consult the postgres manual or
+-- your DBA and give appropriate permissions.
+
+-- grant all privileges on all tables in the public schema. This can be customised:
+-- For example, if you only want to use GraphQL regular queries and not mutations,
+-- then you can set: GRANT SELECT ON ALL TABLES...
+GRANT USAGE ON SCHEMA public TO hasuramanager;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO hasuramanager;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO hasuramanager;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO hasuramanager;
+
+-- Similarly add these for other schemas as well, if you have any.
+-- GRANT USAGE ON SCHEMA <schema-name> TO hasuramanager;
+-- GRANT ALL ON ALL TABLES IN SCHEMA <schema-name> TO hasuramanager;
+-- GRANT ALL ON ALL SEQUENCES IN SCHEMA <schema-name> TO hasuramanager;
+-- GRANT ALL ON ALL FUNCTIONS IN SCHEMA <schema-name> TO hasuramanager;
+
+alter default privileges for user hasuramanager grant all on tables to public;
+alter default privileges for user hasuramanager grant all on sequences to public;
+alter default privileges for user hasuramanager grant all on functions to public;
+alter default privileges for user hasuramanager grant all on types to public;
+alter default privileges for user hasuramanager grant all on schemas to public;
+
+------------------------------------------------------------------------------
+
+-- create a separate user for to access api user database
+CREATE USER hasurauser WITH PASSWORD 'hasurauser';
+
+-- create pgcrypto extension, required for UUID
+-- See section below on pgcrypto in PG search path
+-- CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- The below permissions are optional. This is dependent on what access to your
 -- tables/schemas you want give to hasura. If you want expose the public
@@ -48,8 +84,8 @@ GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO hasurauser;
 -- GRANT ALL ON ALL SEQUENCES IN SCHEMA <schema-name> TO hasurauser;
 -- GRANT ALL ON ALL FUNCTIONS IN SCHEMA <schema-name> TO hasurauser;
 
-alter default privileges for user hasurauser grant all on tables to hasurauser;
-alter default privileges for user hasurauser grant all on sequences to hasurauser;
-alter default privileges for user hasurauser grant all on functions to hasurauser;
-alter default privileges for user hasurauser grant all on types to hasurauser;
-alter default privileges for user hasurauser grant all on schemas to hasurauser;
+-- alter default privileges for user hasurauser grant all on tables to hasurauser;
+-- alter default privileges for user hasurauser grant all on sequences to hasurauser;
+-- alter default privileges for user hasurauser grant all on functions to hasurauser;
+-- alter default privileges for user hasurauser grant all on types to hasurauser;
+-- alter default privileges for user hasurauser grant all on schemas to hasurauser;
---
+++ permissions-builder-1/hasura/migrations/default/1658664418578_set_up_rls/up.sql
@@ -0,0 +1,131 @@
+-- -*- sql-product: postgres; -*-
+
+alter table assignment enable row level security;
+
+alter table organization enable row level security;
+
+-- alter table permission enable row level security;
+
+alter table project enable row level security;
+
+alter table timesheet enable row level security;
+
+-- alter table "user" enable row level security;
+
+-- alter table "user_permission" enable row level security;
+
+create policy query on assignment for select using (true);
+
+create policy query on organization for select using (true);
+
+-- create policy query on permission for select using (true);
+
+create policy query on project for select using (true);
+
+create policy query on timesheet for select using (true);
+
+-- create policy query on "user" for select using (true);
+
+-- create policy query on user_permission for select using (true);
+
+create policy delete_assigned_projects on project for delete using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission
+	    on true
+	    and user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'edit_assigned_projects'
+  )
+  and
+  exists (
+    select
+      1
+      from
+	assignment
+     where true
+       and project_id = project.id
+       and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+  )
+);
+
+create policy edit_all_projects on project for update using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission
+	    on true
+	    and user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'edit_all_projects'
+  )
+);
+
+create policy edit_all_timesheets on timesheet for update using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission
+	    on true
+	    and user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'edit_all_timesheets'
+  )
+);
+
+create policy edit_assigned_projects on project for update using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission
+	    on true
+	    and user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'edit_assigned_projects'
+  )
+  and
+  exists (
+    select
+      1
+      from
+	assignment
+     where true
+       and project_id = project.id
+       and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+  )
+);
+
+create policy view_all_projects on project for select using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission
+	    on true
+	    and user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'view_all_projects'
+  )
+);
+
+create policy view_all_timesheets on timesheet for select using (
+  exists (
+    select
+      1
+      from
+	user_permission
+	join permission on user_permission.permission_id = permission.id
+	    and user_id = (current_setting('hasura.user')::jsonb->>'x-hasura-user-id')::uuid
+	    and permission.name = 'view_all_timesheets'
+  )
+);
