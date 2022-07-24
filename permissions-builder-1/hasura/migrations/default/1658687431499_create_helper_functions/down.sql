b470a92fe133be60419a1470464969a60d588547
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-07-24 11:31:45 -0700

    Added helper functions

---
+++ permissions-builder-1/hasura/migrations/default/1658687431499_create_helper_functions/down.sql
@@ -0,0 +1,45 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create or replace function get_assigned_projects(user_id uuid) returns setof project stable parallel safe
+-- as $$
+--   select
+--   project.*
+--   from project
+--   where true
+--   and exists (
+--     select 1
+--       from
+-- 	assignment
+--      where true
+--        and project_id = project.id
+--        and user_id = $1)
+-- $$ language sql;
+--
+-- create or replace function get_unassigned_projects(user_id uuid) returns setof project stable parallel safe
+-- as $$
+--   select
+--   project.*
+--   from project
+--   where true
+--   or not exists (
+--     select 1
+--       from
+-- 	assignment
+--      where true
+--        and project_id = project.id
+--        and user_id = $1)
+-- $$ language sql;
+--
+-- create or replace function get_assigned_users() returns setof "user" stable parallel safe
+-- as $$
+--   select
+--   *
+--   from "user"
+--   where true
+--   and exists (
+--     select 1
+--       from
+-- 	assignment
+--      where true
+--        and user_id = "user".id)
+-- $$ language sql;
