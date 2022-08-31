6eebcbbf2bba8259efcd1a47301d2a3b7ad6301f
Hasura <hasura@david-desktop>
2022-08-31 14:41:48 -0700

    Replaced the asset_with_traits_as_array view with an m view

---
+++ streaming-subscriptions-1/hasura/migrations/default/1661982097921_replace_with_m_view/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- drop materialized view if exists asset_with_traits_as_array;
+--
+-- drop view if exists asset_with_traits_as_array;
+--
+-- create materialized view if not exists asset_with_traits_as_array as
+--   select
+--     id,
+--     created_at,
+--     updated_at,
+--     name,
+--     string_to_array(traits, ',') as traits
+--     from asset_with_traits;
---
+++ streaming-subscriptions-1/hasura/migrations/default/1661982097921_replace_with_m_view/up.sql
@@ -0,0 +1,12 @@
+drop materialized view if exists asset_with_traits_as_array;
+
+drop view if exists asset_with_traits_as_array;
+
+create materialized view if not exists asset_with_traits_as_array as
+  select
+    id,
+    created_at,
+    updated_at,
+    name,
+    string_to_array(traits, ',') as traits
+    from asset_with_traits;
