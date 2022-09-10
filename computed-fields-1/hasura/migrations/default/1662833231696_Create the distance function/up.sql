e78abd068a4e6d7e700626b00dcff0905454ae68
Hasura <hasura@david-desktop>
2022-09-10 11:07:18 -0700

    Created the distance function

---
+++ computed-fields-1/hasura/migrations/default/1662833231696_Create the distance function/up.sql
@@ -0,0 +1,10 @@
+drop function if exists ch01.restaurants_near cascade;
+
+create or replace function ch01.restaurants_near (lat double precision, lon double precision)
+  returns setof ch01.restaurants_near as $$
+  select
+  restaurants.*,
+  st_distance(st_transform(geom, 3857), st_transform(st_setsrid(st_makepoint(lat, lon), 4326), 3857)) as distance
+  from
+  ch01.restaurants
+$$ language sql stable;
