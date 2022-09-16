54547e7db1042a882d31d223a7a48eaddd07c5d6
Hasura <hasura@david-desktop>
2022-09-16 11:33:36 -0700

    Fix trigger functions

--- building-apps-in-postgres-1/hasura/migrations/default/1663351489913_normalize country/up.sql
+++ building-apps-in-postgres-1/hasura/migrations/default/1663351489913_normalize country/up.sql
@@ -18,13 +18,15 @@ create or replace view account as
       join country on country.id = account.country_id;
 
 create or replace function account_insert () returns trigger as $$
+  declare
+    new_country_id uuid;
   begin
     if exists (select 1 from country where country.name = new.country) then
-      new.country_id := (select id from country where name = new.country);
+      new_country_id := (select id from country where name = new.country);
     else
-      insert into country (name) values (new.country) returning (id) into new.country_id;
+      insert into country (name) values (new.country) returning (id) into new_country_id;
     end if;
-    insert into core.account (name, country_id) values (new.name, new.country_id);
+    insert into core.account (name, country_id) values (new.name, new_country_id);
     return new;
   end;
 $$ language plpgsql;
@@ -43,13 +45,15 @@ create trigger account_delete instead of delete on account
   for each row execute function account_delete();
 
 create or replace function account_update () returns trigger as $$
+  declare
+    new_country_id uuid;
   begin
     if exists (select 1 from country where country.name = new.country) then
-      new.country_id := (select id from country where name = new.country);
+      new_country_id := (select id from country where name = new.country);
     else
-      insert into country (name) values (new.country) returning (id) into new.country_id;
+      insert into country (name) values (new.country) returning (id) into new_country_id;
     end if;
-    update core.account set name = new.name, country_id = new.country_id where id = old.id;
+    update core.account set name = new.name, country_id = new_country_id where id = old.id;
     return new;
   end;
 $$ language plpgsql;
