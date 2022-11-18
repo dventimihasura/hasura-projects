64eec7cb4725491f33a2e094159427d626e86630
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-11-18 11:23:33 -0800

    Created the origin fdw server

---
+++ postgres-read-through-cache-1/migrations/default/1668799401359_create the foreign server from this cache database to the origin database/down.sql
@@ -0,0 +1,3 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create server origin foreign data wrapper postgres_fdw options (host 'origin', dbname 'postgres', port '5432');
---
+++ postgres-read-through-cache-1/migrations/default/1668799401359_create the foreign server from this cache database to the origin database/up.sql
@@ -0,0 +1,1 @@
+create server origin foreign data wrapper postgres_fdw options (host 'origin', dbname 'postgres', port '5432');
