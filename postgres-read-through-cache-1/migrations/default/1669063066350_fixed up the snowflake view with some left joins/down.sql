a062daae44fef1ced2a405169bb24da20d5c2716
David A. Ventimiglia <davidaventimiglia@hasura.io>
2022-11-21 12:37:55 -0800

    Fixed up the snowflake account summary view with some left joins

---
+++ postgres-read-through-cache-1/migrations/default/1669063066350_fixed up the snowflake view with some left joins/down.sql
@@ -0,0 +1,11 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- create or replace view snowflake_account_summary as
+-- select
+--   account.id,
+--   sum(units*price)
+--   from snowflake.account
+--        left join snowflake."invoice" on "invoice".account_id = account.id
+--        left join snowflake.line_item on line_item.invoice_id = "invoice".id
+--        left join snowflake.product on product.id = line_item.product_id
+--  group by account.id;
