f4dc75cb140edaca88a892d0b163c6f5b801069a
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-01-18 13:56:12 -0800

    Added the articles table

---
+++ microsoft-sql-server-poc-1/migrations/default/1674078889489_create_articles_table /down.sql
@@ -0,0 +1,7 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TABLE articles (
+--   id              INT           NOT NULL    IDENTITY    PRIMARY KEY,
+--   title           VARCHAR(100)  NULL,
+--   content         VARCHAR(100)  NULL
+-- );
---
+++ microsoft-sql-server-poc-1/migrations/default/1674078889489_create_articles_table /up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE articles (
+  id              INT           NOT NULL    IDENTITY    PRIMARY KEY,
+  title           VARCHAR(100)  NULL,
+  content         VARCHAR(100)  NULL
+);
