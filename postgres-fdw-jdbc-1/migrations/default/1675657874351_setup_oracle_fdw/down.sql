21d61c471e6d52d4dc55506d6afce67ece4e8aaf
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-02-05 20:49:52 -0800

    Switching away from JDBC FDWs

--- postgres-fdw-jdbc-1/migrations/default/1675657874351_setup_oracle_fdw/down.sql
+++ postgres-fdw-jdbc-1/migrations/default/1675657874351_setup_oracle_fdw/down.sql
@@ -1,5 +1,1 @@
 drop schema if exists oracle cascade;
-
-drop server if exists oracle cascade;
-
-drop user mapping if exists for current_user server oracle;
--- postgres-fdw-jdbc-1/migrations/default/1675657874351_setup_oracle_fdw/up.sql
+++ postgres-fdw-jdbc-1/migrations/default/1675657874351_setup_oracle_fdw/up.sql
@@ -1,9 +1,3 @@
 create schema if not exists oracle;
 
-create extension if not exists oracle_fdw;
 
-create server if not exists oracle foreign data wrapper oracle_fdw options (dbserver '//oracle:1521/xe');
-
-create user mapping if not exists for current_user server oracle options (user 'system', password 'chinook');
-
-import foreign schema "SYS" limit to (album, artist, customer, employee, genre, invoice, invoiceline, mediatype, playlist, playlisttrack, track) from server oracle into oracle options (case 'lower');
