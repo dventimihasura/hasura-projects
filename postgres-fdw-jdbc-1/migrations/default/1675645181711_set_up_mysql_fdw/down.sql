e8cfb83ae75724e96cce5f537ceb928831988e98
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-02-05 19:14:40 -0800

    Fixing the oracle fdw

--- postgres-fdw-jdbc-1/migrations/default/1675645181711_set_up_mysql_fdw/down.sql
+++ postgres-fdw-jdbc-1/migrations/default/1675645181711_set_up_mysql_fdw/down.sql
@@ -1,19 +1,6 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- drop server if exists mysql cascade;
---
--- create server if not exists mysql foreign data wrapper jdbc_fdw options(
---   drivername 'org.mariadb.jdbc.Driver',
---   url 'jdbc:mysql://mysql:3306/chinook',
---   jarfile '/usr/share/java/mariadb-java-client.jar'
--- );
---
--- drop user mapping if exists for current_user server mysql;
---
--- create user mapping if not exists for current_user server mysql options(username 'chinook', password 'chinook');
---
--- drop schema if exists mysql cascade;
---
--- create schema if not exists mysql;
---
--- import foreign schema mysql limit to (album, artist, customer, employee, genre, invoice, invoiceline, mediatype, playlist, playlisttrack, track) from server mysql into mysql;
+drop server if exists mysql cascade;
+
+drop user mapping if exists for current_user server mysql;
+
+drop schema if exists mysql cascade;
+
--- postgres-fdw-jdbc-1/migrations/default/1675645181711_set_up_mysql_fdw/up.sql
+++ postgres-fdw-jdbc-1/migrations/default/1675645181711_set_up_mysql_fdw/up.sql
@@ -1,16 +1,10 @@
-drop server if exists mysql cascade;
-
 create server if not exists mysql foreign data wrapper jdbc_fdw options(
   drivername 'org.mariadb.jdbc.Driver',
   url 'jdbc:mysql://mysql:3306/chinook',
   jarfile '/usr/share/java/mariadb-java-client.jar'
 );
 
-drop user mapping if exists for current_user server mysql;
-
 create user mapping if not exists for current_user server mysql options(username 'chinook', password 'chinook');
-
-drop schema if exists mysql cascade;
 
 create schema if not exists mysql;
 
