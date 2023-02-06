da5b69029c51221c2b2e8a40ce59c9eae6cb3fd0
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-02-05 20:29:40 -0800

    Fixing the oracle fdw

--- postgres-fdw-jdbc-1/migrations/default/1675643470919_init/up.sql
+++ postgres-fdw-jdbc-1/migrations/default/1675643470919_init/up.sql
@@ -1,5 +1,5 @@
-SET check_function_bodies = false;
-CREATE EXTENSION IF NOT EXISTS jdbc_fdw WITH SCHEMA public;
-COMMENT ON EXTENSION jdbc_fdw IS 'foreign-data wrapper for remote servers available over JDBC';
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+create extension if not exists jdbc_fdw with schema public;
+
+create extension if not exists pgcrypto with schema public;
+
