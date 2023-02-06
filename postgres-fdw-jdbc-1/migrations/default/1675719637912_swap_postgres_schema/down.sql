f51dfc38349d4528576e28db277bb9ab808e434b
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-02-06 13:51:29 -0800

    Moving the furniture

---
+++ postgres-fdw-jdbc-1/migrations/default/1675719637912_swap_postgres_schema/down.sql
@@ -0,0 +1,3 @@
+alter schema postgres rename to postgres2;
+
+alter schema postgres_old rename to postgres;
---
+++ postgres-fdw-jdbc-1/migrations/default/1675719637912_swap_postgres_schema/up.sql
@@ -0,0 +1,3 @@
+alter schema postgres rename to postgres_old;
+
+alter schema postgres2 rename to postgres;
