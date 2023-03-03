b5209e3027b581d69a8eb657f0d649d95fffaab7
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-03-03 12:44:48 -0800

    Added sprocs

--- postgres-fdw-mssql-1/init_mssql.sql
+++ postgres-fdw-mssql-1/init_mssql.sql
@@ -11,3 +11,31 @@ go
 
 insert into person (name) values ('David'), ('Daniel'), ('Tia');
 go
+
+create function get_persons ()
+  returns table
+as
+  return select id, name from person;
+go
+
+create sequence countby1 start with 1 increment by 1 ;  
+go
+
+create procedure get_sequence_id
+as
+begin
+  select next value for countby1 as id;
+  return;
+end;
+go
+
+create procedure get_guid_id
+as
+begin
+  select newid() as id;
+  return;
+end;
+go
+
+waitfor delay '02:00';
+go
