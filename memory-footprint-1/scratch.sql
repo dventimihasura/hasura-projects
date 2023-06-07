381d88e3871855632ff13e42d30de615cb763289
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-06-07 16:37:18 -0700

    Got replace_metadata to work

--- memory-footprint-1/scratch.sql
+++ memory-footprint-1/scratch.sql
@@ -7,22 +7,66 @@ select
       generate_series(1, 1) i) foo;
 
 select
-  create_table(schema_name, 10, i)
+  create_table(schema_name, 2, i)
   from (
     select
       schema_name,
-      generate_series(1, 10) i
+      generate_series(1, 2) i
       from
 	information_schema.schemata
      where schema_name like 'test_%') foo;
 
-select generate_series(1, 3);
+select
+  jsonb_pretty(
+    jsonb_build_object(
+      'type', 'bulk',
+      'args', jsonb_build_array(
+	jsonb_build_object(
+	  'type', 'replace_metadata',
+	  'args', jsonb_build_object(
+	    'sources', jsonb_agg(x))))))
+  from (
+    select
+      jsonb_build_object(
+	'configuration', '{"connection_info":{"database_url":{"from_env":"HASURA_GRAPHQL_DATABASE_URL"},"isolation_level":"read-committed","pool_settings":{"connection_lifetime":600,"idle_timeout":180,"max_connections":50,"retries":1},"use_prepared_statements":true}}'::jsonb,
+	'name', catalog_name,
+	'kind', 'postgres',
+	'tables', (
+	  select
+	    jsonb_agg(x)
+	    from (
+	      select
+		jsonb_build_object(
+		  'select_permissions', (
+		    select
+		      jsonb_agg(x)
+		      from (
+			select
+			  jsonb_build_object(
+			    'role', format('role_%s', generate_series),
+			    'permission', jsonb_build_object(
+			      'filter', jsonb_build_object(),
+			      'columns', (
+				select
+				  jsonb_agg(x)
+				  from (
+				    select
+				      column_name x
+				      from
+					information_schema.columns
+				     where table_name = tables.table_name) foo))) x
+			  from
+			    generate_series(1, 2)) foo),
+			    'table', jsonb_build_object(
+			      'name', table_name,
+			      'schema', table_schema)) x
+		from
+		  information_schema.tables
+	       where true
+		 and table_name like 'test_%'
+		 and catalog_name = information_schema_catalog_name.catalog_name) foo)) x
+      from
+	information_schema.information_schema_catalog_name) foo;
 
-create table idx (i int);
 
-insert into idx select generate_series(1, 3);
-
-select i from idx;
-
-select create_permissions(format('role_%s', x)) from generate_series(11, 15) x;
 
