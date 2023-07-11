b9653c2b0786f83e717fd83c5c32fe0a2fb10b62
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-07-11 11:25:07 -0700

    Added the CPU docker file

--- hasura-mmlib-2/initdb.d-postgres/01_init_model.sql
+++ hasura-mmlib-2/initdb.d-postgres/01_init_model.sql
@@ -1,6 +1,8 @@
 -- -*- sql-product: postgres; -*-
 
-create extension citext;
+create extension if not exists citext;
+
+create extension if not exists vector;
 
 create domain email as citext
   check ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
@@ -25,5 +27,13 @@ create table resume (
   resume_str text,
   resume_html text,
   category text);
-  
-alter table resume add column embedding vector(1024) null;
+
+alter table resume add column embedding vector(1024) generated always as (pgml.embed('intfloat/e5-large', 'passage: ' || resume_str)) stored;
+
+create index on resume using ivfflat (embedding vector_l2_ops) with (lists = 3);
+
+create index on resume using ivfflat (embedding vector_ip_ops) with (lists = 3);
+
+create index on resume using ivfflat (embedding vector_cosine_ops) with (lists = 3);
+
+copy resume(resume_id, resume_str, resume_html, category) from program 'unzip -p /docker-entrypoint-initdb.d/archive.zip Resume/Resume.csv' with (format csv, header true, quote '"');
