6bfa2935a645d356c8f40af67e2ca2ebee5a7314
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-07-18 14:51:32 -0700

    Got auto-migrations to work

--- yb-test-2/locustfile.py
+++ yb-test-2/locustfile.py
@@ -1,44 +1,29 @@
 import time
 from locust import HttpUser, task, between
 
-class QuickstartUser(HttpUser):
-    # @task
-    # def healthcheck(self):
-    #     self.client.get("/healthz")
+class PerfTest(HttpUser):
+    @task
+    def nginx(self):
+        self.client.get(
+            "http://localhost/")
 
     @task
-    def product(self):
-        with self.client.post("/v1/graphql",
-                         json={"query": '''
-query MyQuery {
-  product(limit: 1) {
-    id
-  }
-}
-'''},
-                         catch_response=True) as response:
-            if response.elapsed.total_seconds() > 2:
-                response.failure("Request took too long")
+    def hasura_healthcheck(self):
+        self.client.get(
+            "http://localhost:8080/healthz")
 
-#     @task
-#     def product(self):
-#         body = '''
-# query MyQuery {
-#   account(limit: 10) {
-#     id
-#     name
-#   }
-# }
-# '''
-#         # self.client.post("/v1/graphql",
-#         #                  json={"query": body},
-#         #                  catch_response=True) as response:
-#         with self.client.post("/v1/graphql",
-#                               json={"query": body},
-#                               catch_response=True) as response:
-#             if response.elapsed.total_seconds() > 2:
-#                 response.failure("Request took too long")
-
+    @task
+    def hasura_rest(self):
+        with self.client.post(
+                "http://localhost:8080/v1/graphql/perftest",
+                catch_response=True, verify=False, headers={"x-hasura-admin-secret": "fake-one"}) as response:
+            try:
+                if response.elapsed.total_seconds() > 1:
+                    response.failure(f"[{dt.now()}, x-requiest-id is {response.headers['x-request-id']}, status_code: {response.status_code}] Response took too long: {response.elapsed.total_seconds()}")
+                elif response.status_code != 200:
+                    response.failure(f"Bad status code {response.status_code}")
+            except Exception as e:
+                response.failure("{e}")
 
     
     
