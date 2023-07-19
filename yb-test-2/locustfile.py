530a45595a1f0db8e367d8edc11efdffb659b71d
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-07-19 10:10:24 -0700

    Commented out nginx

--- yb-test-2/locustfile.py
+++ yb-test-2/locustfile.py
@@ -2,28 +2,28 @@ import time
 from locust import HttpUser, task, between
 
 class PerfTest(HttpUser):
-    @task
-    def nginx(self):
-        self.client.get(
-            "http://localhost/")
+    # @task
+    # def nginx(self):
+    #     self.client.get(
+    #         "/")
 
     @task
     def hasura_healthcheck(self):
         self.client.get(
-            "http://localhost:8080/healthz")
+            "/healthz")
 
-    @task
-    def hasura_rest(self):
-        with self.client.post(
-                "http://localhost:8080/v1/graphql/perftest",
-                catch_response=True, verify=False, headers={"x-hasura-admin-secret": "fake-one"}) as response:
-            try:
-                if response.elapsed.total_seconds() > 1:
-                    response.failure(f"[{dt.now()}, x-requiest-id is {response.headers['x-request-id']}, status_code: {response.status_code}] Response took too long: {response.elapsed.total_seconds()}")
-                elif response.status_code != 200:
-                    response.failure(f"Bad status code {response.status_code}")
-            except Exception as e:
-                response.failure("{e}")
+    # @task
+    # def hasura_rest(self):
+    #     with self.client.post(
+    #             "/api/rest/perftest",
+    #             catch_response=True, verify=False, headers={"x-hasura-admin-secret": "fake-one"}) as response:
+    #         try:
+    #             if response.elapsed.total_seconds() > 1:
+    #                 response.failure(f"[{dt.now()}, x-requiest-id is {response.headers['x-request-id']}, status_code: {response.status_code}] Response took too long: {response.elapsed.total_seconds()}")
+    #             elif response.status_code != 200:
+    #                 response.failure(f"Bad status code {response.status_code}")
+    #         except Exception as e:
+    #             response.failure("{e}")
 
     
     
