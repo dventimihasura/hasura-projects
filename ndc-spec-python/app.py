f07ea4591d8a217d4b15108d411d368acd2eeed2
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-09-26 15:34:37 -0700

    Used JSON output instead of String

--- ndc-spec-python/app.py
+++ ndc-spec-python/app.py
@@ -20,247 +20,240 @@ def metrics():
 
 @app.get("/capabilities")
 def capabilities():
-    return """
-{
-  "versions": "^0.1.0",
-  "capabilities": {
-    "query": {
-      "relation_comparisons": {},
-      "order_by_aggregate": {},
-      "foreach": {}
-    },
-    "mutations": {
-      "nested_inserts": {},
-      "returning": {}
-    },
-    "relationships": {}
-  }
-}
-    """
+    return {
+        "versions": "^0.1.0",
+        "capabilities": {
+            "query": {
+                "relation_comparisons": {},
+                "order_by_aggregate": {},
+                "foreach": {}
+            },
+            "mutations": {
+                "nested_inserts": {},
+                "returning": {}
+            },
+            "relationships": {}
+        }
+    }
 
 
 @app.get("/schema")
 def schema():
-    return """
-{
-  "scalar_types": {
-    "Int": {
-      "aggregate_functions": {
-        "max": {
-          "result_type": {
-            "type": "nullable",
-            "underlying_type": {
-              "type": "named",
-              "name": "Int"
+    return {
+        "scalar_types": {
+            "Int": {
+                "aggregate_functions": {
+                    "max": {
+                        "result_type": {
+                            "type": "nullable",
+                            "underlying_type": {
+                                "type": "named",
+                                "name": "Int"
+                            }
+                        }
+                    },
+                    "min": {
+                        "result_type": {
+                            "type": "nullable",
+                            "underlying_type": {
+                                "type": "named",
+                                "name": "Int"
+                            }
+                        }
+                    }
+                },
+                "comparison_operators": {},
+                "update_operators": {}
+            },
+            "String": {
+                "aggregate_functions": {},
+                "comparison_operators": {
+                    "like": {
+                        "argument_type": {
+                            "type": "named",
+                            "name": "String"
+                        }
+                    }
+                },
+                "update_operators": {}
             }
-          }
         },
-        "min": {
-          "result_type": {
-            "type": "nullable",
-            "underlying_type": {
-              "type": "named",
-              "name": "Int"
+        "object_types": {
+            "article": {
+                "description": "An article",
+                "fields": {
+                    "author_id": {
+                        "description": "The article's author ID",
+                        "type": {
+                            "type": "named",
+                            "name": "Int"
+                        }
+                    },
+                    "id": {
+                        "description": "The article's primary key",
+                        "type": {
+                            "type": "named",
+                            "name": "Int"
+                        }
+                    },
+                    "title": {
+                        "description": "The article's title",
+                        "type": {
+                            "type": "named",
+                            "name": "String"
+                        }
+                    }
+                }
+            },
+            "author": {
+                "description": "An author",
+                "fields": {
+                    "first_name": {
+                        "description": "The author's first name",
+                        "type": {
+                            "type": "named",
+                            "name": "String"
+                        }
+                    },
+                    "id": {
+                        "description": "The author's primary key",
+                        "type": {
+                            "type": "named",
+                            "name": "Int"
+                        }
+                    },
+                    "last_name": {
+                        "description": "The author's last name",
+                        "type": {
+                            "type": "named",
+                            "name": "String"
+                        }
+                    }
+                }
             }
-          }
-        }
-      },
-      "comparison_operators": {},
-      "update_operators": {}
-    },
-    "String": {
-      "aggregate_functions": {},
-      "comparison_operators": {
-        "like": {
-          "argument_type": {
-            "type": "named",
-            "name": "String"
-          }
-        }
-      },
-      "update_operators": {}
-    }
-  },
-  "object_types": {
-    "article": {
-      "description": "An article",
-      "fields": {
-        "author_id": {
-          "description": "The article's author ID",
-          "type": {
-            "type": "named",
-            "name": "Int"
-          }
         },
-        "id": {
-          "description": "The article's primary key",
-          "type": {
-            "type": "named",
-            "name": "Int"
-          }
-        },
-        "title": {
-          "description": "The article's title",
-          "type": {
-            "type": "named",
-            "name": "String"
-          }
-        }
-      }
-    },
-    "author": {
-      "description": "An author",
-      "fields": {
-        "first_name": {
-          "description": "The author's first name",
-          "type": {
-            "type": "named",
-            "name": "String"
-          }
-        },
-        "id": {
-          "description": "The author's primary key",
-          "type": {
-            "type": "named",
-            "name": "Int"
-          }
-        },
-        "last_name": {
-          "description": "The author's last name",
-          "type": {
-            "type": "named",
-            "name": "String"
-          }
-        }
-      }
+        "collections": [
+            {
+                "name": "articles",
+                "description": "A collection of articles",
+                "arguments": {},
+                "type": "article",
+                "deletable": False,
+                "uniqueness_constraints": {
+                    "ArticleByID": {
+                        "unique_columns": [
+                            "id"
+                        ]
+                    }
+                },
+                "foreign_keys": {}
+            },
+            {
+                "name": "authors",
+                "description": "A collection of authors",
+                "arguments": {},
+                "type": "author",
+                "deletable": False,
+                "uniqueness_constraints": {
+                    "AuthorByID": {
+                        "unique_columns": [
+                            "id"
+                        ]
+                    }
+                },
+                "foreign_keys": {}
+            },
+            {
+                "name": "articles_by_author",
+                "description": "Articles parameterized by author",
+                "arguments": {
+                    "author_id": {
+                        "type": {
+                            "type": "named",
+                            "name": "Int"
+                        }
+                    }
+                },
+                "type": "article",
+                "deletable": False,
+                "uniqueness_constraints": {},
+                "foreign_keys": {}
+            }
+        ],
+        "functions": [
+            {
+                "name": "latest_article_id",
+                "description": "Get the ID of the most recent article",
+                "arguments": {},
+                "result_type": {
+                    "type": "nullable",
+                    "underlying_type": {
+                        "type": "named",
+                        "name": "Int"
+                    }
+                }
+            }
+        ],
+        "procedures": [
+            {
+                "name": "upsert_article",
+                "description": "Insert or update an article",
+                "arguments": {
+                    "article": {
+                        "description": "The article to insert or update",
+                        "type": {
+                            "type": "named",
+                            "name": "article"
+                        }
+                    }
+                },
+                "result_type": {
+                    "type": "Noneable",
+                    "underlying_type": {
+                        "type": "named",
+                        "name": "article"
+                    }
+                }
+            }
+        ]
     }
-  },
-  "collections": [
-    {
-      "name": "articles",
-      "description": "A collection of articles",
-      "arguments": {},
-      "type": "article",
-      "deletable": false,
-      "uniqueness_constraints": {
-        "ArticleByID": {
-          "unique_columns": [
-            "id"
-          ]
-        }
-      },
-      "foreign_keys": {}
-    },
-    {
-      "name": "authors",
-      "description": "A collection of authors",
-      "arguments": {},
-      "type": "author",
-      "deletable": false,
-      "uniqueness_constraints": {
-        "AuthorByID": {
-          "unique_columns": [
-            "id"
-          ]
-        }
-      },
-      "foreign_keys": {}
-    },
-    {
-      "name": "articles_by_author",
-      "description": "Articles parameterized by author",
-      "arguments": {
-        "author_id": {
-          "type": {
-            "type": "named",
-            "name": "Int"
-          }
-        }
-      },
-      "type": "article",
-      "deletable": false,
-      "uniqueness_constraints": {},
-      "foreign_keys": {}
-    }
-  ],
-  "functions": [
-    {
-      "name": "latest_article_id",
-      "description": "Get the ID of the most recent article",
-      "arguments": {},
-      "result_type": {
-        "type": "nullable",
-        "underlying_type": {
-          "type": "named",
-          "name": "Int"
-        }
-      }
-    }
-  ],
-  "procedures": [
-    {
-      "name": "upsert_article",
-      "description": "Insert or update an article",
-      "arguments": {
-        "article": {
-          "description": "The article to insert or update",
-          "type": {
-            "type": "named",
-            "name": "article"
-          }
-        }
-      },
-      "result_type": {
-        "type": "nullable",
-        "underlying_type": {
-          "type": "named",
-          "name": "article"
-        }
-      }
-    }
-  ]
-}
-"""
 
 
 @app.post("/query")
 def query():
-    return """
-[
-  {
-    "rows": [
-      {
-        "ine_": {
-          "title": "Row Field Value"
-        },
-        "elit4": {
-          "title": "Row Field Value"
+    return [
+        {
+            "rows": [
+                {
+                    "ine_": {
+                        "title": "Row Field Value"
+                    },
+                    "elit4": {
+                        "title": "Row Field Value"
+                    }
+                },
+                {
+                    "est_ae": {
+                        "title": "Row Field Value"
+                    },
+                    "commodo_16b": {
+                        "title": "Row Field Value"
+                    }
+                }
+            ],
+            "aggregates": None,
+            "dolor__13": "veniam",
+            "dolor32": -44004488.342306614
         }
-      },
-      {
-        "est_ae": {
-          "title": "Row Field Value"
-        },
-        "commodo_16b": {
-          "title": "Row Field Value"
-        }
-      }
-    ],
-    "aggregates": null,
-    "dolor__13": "veniam",
-    "dolor32": -44004488.342306614
-  }
-]
-    """
+    ]
 
 
 @app.post("/explain")
 def explain():
-    return """
-{
-  "details": {
-    "proident_6": "velit ut in qui",
-    "voluptate_42": "sint ipsum cillum ea reprehenderit"
-  }
-}
-    """
+    return {
+        "details": {
+            "proident_6": "velit ut in qui",
+            "voluptate_42": "sint ipsum cillum ea reprehenderit"
+        }
+    }
+
