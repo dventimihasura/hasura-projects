f2a6276e88b5ef12f9726b6d6326624e1c90cc06
David A. Ventimiglia <davidaventimiglia@gmail.com>
2023-11-02 11:12:15 -0700

    Replaced the drop statements with create databases

--- multi-role-1/docker-entrypoint-initdb.d/000_drop_objects.sql
+++ multi-role-1/docker-entrypoint-initdb.d/000_drop_objects.sql
@@ -1,15 +1,11 @@
 -- -*- sql-product: postgres; -*-
 
-drop table if exists account cascade;
+create database metadata1;
 
-drop table if exists "order" cascade;
+create database metadata2;
 
-drop table if exists order_detail cascade;
+create database metadata3;
 
-drop table if exists product cascade;
+create database metadata4;
 
-drop table if exists region cascade;
-
-drop function if exists product_search(text);
-
-drop function if exists product_search_slow(text, real);
+create database metadata5;
