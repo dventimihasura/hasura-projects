8b419ded18f9082bca528c5512023dd219788db0
David A. Ventimiglia <davidaventimiglia@gmail.com>
2023-11-02 15:02:01 -0700

    Moving the furniture

--- multi-role-1/docker-entrypoint-initdb.d/000_drop_objects.sql
+++ multi-role-1/docker-entrypoint-initdb.d/000_drop_objects.sql
@@ -1,11 +1,2 @@
 -- -*- sql-product: postgres; -*-
 
-create database metadata1;
-
-create database metadata2;
-
-create database metadata3;
-
-create database metadata4;
-
-create database metadata5;
