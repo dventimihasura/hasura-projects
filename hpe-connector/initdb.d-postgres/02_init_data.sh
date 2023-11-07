397ec3870bf6d2c2d2370f75474ecbbaf02839b6
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-11-07 13:40:48 -0800

    Fixed up the openapi spec

--- hpe-connector/initdb.d-postgres/02_init_data.sh
+++ hpe-connector/initdb.d-postgres/02_init_data.sh
@@ -2,3 +2,5 @@
 
 psql -c "copy core.server (content) from stdin;" < /docker-entrypoint-initdb.d/servers.jsonl
 psql -c "copy core.alert (content) from stdin;" < /docker-entrypoint-initdb.d/alerts.jsonl
+echo "\lo_import /docker-entrypoint-initdb.d/openapi3.json" | psql -U postgres
+
