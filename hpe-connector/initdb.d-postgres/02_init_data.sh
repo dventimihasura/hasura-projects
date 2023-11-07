2d32ed975664f03ba998b503713d5faefc3f4afa
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-11-07 11:44:29 -0800

    Added data

--- hpe-connector/initdb.d-postgres/02_init_data.sh
+++ hpe-connector/initdb.d-postgres/02_init_data.sh
@@ -1,3 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 
-# curl -s -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" "http://prism:4010/compute-ops/v1beta2/servers?[1-100]" | jq -cr '.items[]' | psql "postgres://postgres:postgrespassword@localhost:5432/postgres" -c "copy core.server (content) from stdin;"
+psql -c "copy core.server (content) from stdin;" < /docker-entrypoint-initdb.d/servers.jsonl
+psql -c "copy core.alert (content) from stdin;" < /docker-entrypoint-initdb.d/alerts.jsonl
