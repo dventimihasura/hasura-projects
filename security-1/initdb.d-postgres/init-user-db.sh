415060f401fe290b8c040e6a12d64af308bc5e94
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-11-19 15:30:34 -0800

    Added the Makefile

--- security-1/initdb.d-postgres/init-user-db.sh
+++ security-1/initdb.d-postgres/init-user-db.sh
@@ -3,10 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 <<-EOF
 create role ${METADATA_USER} nosuperuser nocreatedb nocreaterole noinherit login noreplication nobypassrls connection limit 10 password '${METADATA_PASSWORD}' valid until '$(date -d '1 week hence' --rfc-3339=date -u)';
-create role ${DATA_DEFAULT_USER} nosuperuser nocreatedb nocreaterole noinherit login noreplication nobypassrls connection limit 10 password '${DATA_DEFAULT_PASSWORD}' valid until '$(date -d '1 week hence' --rfc-3339=date -u)';
-create role ${DATA_ADMIN_USER} nosuperuser nocreatedb nocreaterole noinherit login noreplication nobypassrls connection limit 10 password '${DATA_ADMIN_PASSWORD}' valid until '$(date -d '1 week hence' --rfc-3339=date -u)';
 create database metadata connection limit = 10;
-create database defaultdb connection limit = 10;
 grant all privileges on database metadata to ${METADATA_USER};
-grant all privileges on database defaultdb to ${DATA_DEFAULT_USER};
 EOF
