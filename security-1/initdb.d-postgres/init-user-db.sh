633b154536d8610f88ea947d8d46918be6cbc6a5
David A. Ventimiglia <davidaventimiglia@hasura.io>
2023-11-20 09:37:12 -0800

    Simplified dramatically

--- security-1/initdb.d-postgres/init-user-db.sh
+++ security-1/initdb.d-postgres/init-user-db.sh
@@ -2,7 +2,4 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 <<-EOF
-create role ${METADATA_USER} nosuperuser nocreatedb nocreaterole noinherit login noreplication nobypassrls connection limit 10 password '${METADATA_PASSWORD}' valid until '$(date -d '1 week hence' --rfc-3339=date -u)';
-create database metadata connection limit = 10;
-grant all privileges on database metadata to ${METADATA_USER};
 EOF
