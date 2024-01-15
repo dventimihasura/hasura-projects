3597d1c999ea66f741848cfa3e270a15bed41fa4
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-01-15 16:10:36 -0600

    Using an unlogged table

--- change-data-capture-1/initdb.d-postgres/03_cdc.sql
+++ change-data-capture-1/initdb.d-postgres/03_cdc.sql
@@ -1,47 +1,52 @@
 -- -*- sql-product: postgres; -*-
 
-select * from pg_create_logical_replication_slot('test', 'wal2json', false, false);
+select * from pg_create_logical_replication_slot('cdc', 'wal2json', false, false);
 
--- create table change (
---   lsn pg_lsn primary key,
---   xid bigint not null,
---   payload jsonb
--- );
-
--- create index on change (xid);
-
--- create extension pg_cron;
-
--- select
---   cron.schedule(
---     '* * * * *',
---     $sql$
---     insert into change
---     select
+-- create or replace view change as
+--   select
 --     lsn,
---     (xid::text)::bigint,
---     data::jsonb
---     from pg_logical_slot_get_changes(
---       'test',
---       null,
---       null,
---       'filter-tables', 'public.change,cron.*,hdb_catalog.*',
---       'include-timestamp', 'true'
---     )
---     where jsonb_array_length(data::jsonb->'change')>0
---     $sql$);
+--     (xid::text)::bigint as xid,
+--     data::jsonb as payload
+--     from
+--       pg_logical_slot_peek_changes(
+-- 	'cdc',
+-- 	null,
+-- 	null,
+-- 	'filter-tables', 'hdb_catalog.*',
+-- 	'format-version', '1',
+-- 	'include-pk', 'true',
+-- 	'include-timestamp', 'true'
+--       )
+--    where
+--      jsonb_array_length(data::jsonb->'change')>0;
 
-create or replace view change as
-  select
+create unlogged table change (
+  lsn pg_lsn primary key,
+  xid bigint not null,
+  payload jsonb
+);
+
+create index on change (xid);
+
+create extension pg_cron;
+
+select
+  cron.schedule(
+    '* * * * *',
+    $sql$
+    insert into change
+    select
     lsn,
-    (xid::text)::bigint as xid,
-    data::jsonb as payload
-    from
-      pg_logical_slot_peek_changes(
-	'test',
-	null,
-	null,
-	'include-timestamp', 'true',
-	'filter-tables', 'hdb_catalog.*')
-   where
-     jsonb_array_length(data::jsonb->'change')>0;
+    (xid::text)::bigint,
+    data::jsonb
+    from pg_logical_slot_get_changes(
+      'cdc',
+      null,
+      null,
+      'filter-tables', 'cron.*,hdb_catalog.*',
+      'format-version', '1',
+      'include-pk', 'true',
+      'include-timestamp', 'true'
+    )
+    where jsonb_array_length(data::jsonb->'change')>0
+    $sql$);
