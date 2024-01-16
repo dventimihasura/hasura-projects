9620088f3629699a263455689cd86e2187590fa2
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-01-16 14:38:09 -0600

    Replaced pg_cron with Hasura scheduled triggers

--- change-data-capture-1/initdb.d-postgres/03_cdc.sql
+++ change-data-capture-1/initdb.d-postgres/03_cdc.sql
@@ -28,29 +28,55 @@ create unlogged table change (
 
 create index on change (lsn);
 
-create extension pg_cron;
+create or replace function cdc ()
+  returns setof change
+  language sql
+  volatile as $sql$
+  insert into change
+  select
+  lsn,
+  (xid::text)::bigint,
+  data::jsonb
+  from pg_logical_slot_get_changes(
+    'cdc',
+    null,
+    null,
+    'filter-tables', 'public.change,cron.*,hdb_catalog.*,faker.*',
+    'format-version', '1',
+    'include-pk', 'true',
+    'include-timestamp', 'true'
+  )
+  where jsonb_array_length(data::jsonb->'change')>0;
+  select pg_replication_slot_advance(
+    'cdc',
+    confirmed_flush_lsn
+  ) from pg_replication_slots;
+  select (null, null, null)::change;
+  $sql$;
 
-select
-  cron.schedule(
-    '* * * * *',
-    $sql$
-    insert into change
-    select
-    lsn,
-    (xid::text)::bigint,
-    data::jsonb
-    from pg_logical_slot_get_changes(
-      'cdc',
-      null,
-      null,
-      'filter-tables', 'public.change,cron.*,hdb_catalog.*,faker.*',
-      'format-version', '1',
-      'include-pk', 'true',
-      'include-timestamp', 'true'
-    )
-    where jsonb_array_length(data::jsonb->'change')>0;
-    select pg_replication_slot_advance(
-      'cdc',
-      confirmed_flush_lsn
-    ) from pg_replication_slots;
-    $sql$);
+-- create extension pg_cron;
+
+-- select
+--   cron.schedule(
+--     '* * * * *',
+--     $sql$
+--     insert into change
+--     select
+--     lsn,
+--     (xid::text)::bigint,
+--     data::jsonb
+--     from pg_logical_slot_get_changes(
+--       'cdc',
+--       null,
+--       null,
+--       'filter-tables', 'public.change,cron.*,hdb_catalog.*,faker.*',
+--       'format-version', '1',
+--       'include-pk', 'true',
+--       'include-timestamp', 'true'
+--     )
+--     where jsonb_array_length(data::jsonb->'change')>0;
+--     select pg_replication_slot_advance(
+--       'cdc',
+--       confirmed_flush_lsn
+--     ) from pg_replication_slots;
+--     $sql$);
