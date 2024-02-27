fe7c2d2bb199f839c6b4b08f567bd0ea79391618
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-02-27 14:17:08 -0800

    Adjusting the permissions

--- netflix-dgs-java-1/initdb.d-postgres/00_metadata.sql
+++ netflix-dgs-java-1/initdb.d-postgres/00_metadata.sql
@@ -3,3 +3,10 @@
 create database graphql_engine_1;
 
 create database graphql_engine_2;
+
+create or replace view statement as
+  select
+    *
+    from
+      pg_stat_statements
+      join pg_roles on pg_roles.oid = userid;
--- netflix-dgs-java-1/initdb.d-postgres/05_add_views.sql
+++
@@ -1,8 +0,0 @@
--- -*- sql-product: postgres; -*-
-
-create or replace view statement as
-  select
-    *
-    from
-      pg_stat_statements
-      join pg_roles on pg_roles.oid = userid;
