3f34d0840c2dfdaaf2c5fa69616ab99a0bfb9074
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-02-27 14:43:04 -0800

    Adjusting the permissions

--- netflix-dgs-java-1/initdb.d-postgres/03_init_users.sql
+++ netflix-dgs-java-1/initdb.d-postgres/03_init_users.sql
@@ -7,7 +7,13 @@ grant connect on database postgres
 
 grant usage on schema public to readonly;
 
+grant usage on schema pg_catalog to readonly;
+
 grant select on all tables in schema public to readonly;
+
+grant select on all tables in schema pg_catalog to readonly;
+
+grant pg_read_all_stats to readonly;
 
 create user read_user with 
   password 'read_user';
@@ -21,10 +27,18 @@ grant connect on database postgres
 
 grant usage on schema public to readwrite;
 
+grant usage on schema pg_catalog to readwrite;
+
 grant all privileges on all tables in schema public to readwrite;
+
+grant all privileges on all tables in schema pg_catalog to readwrite;
 
 create user write_user with 
   password 'write_user';
 
 grant readwrite to write_user;
+
+grant pg_read_all_stats to read_user;
+
+grant pg_read_all_stats to write_user;
 
