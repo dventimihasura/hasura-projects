54ca82099c0ff375afc50ba1357212953fbf5d79
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-03-05 13:36:21 -0800

    Starting over

--- graphql-java-1/src/main/java/io/hasura/graphqljava1/model/AbstractModel.java
+++
@@ -1,9 +0,0 @@
-package io.hasura.netflixdgsjava1.model;
-
-import com.google.gson.*;
-
-public abstract class AbstractModel {
-    public String toString() {
-	return new Gson().toJson(this);
-    }
-}
