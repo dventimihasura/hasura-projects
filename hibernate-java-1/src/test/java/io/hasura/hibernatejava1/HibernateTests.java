5fbf709558d6585303ca4e32d051e85f7be1044e
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-03-21 10:48:34 -0700

    Added another EntityGraph test

--- hibernate-java-1/src/test/java/io/hasura/hibernatejava1/HibernateTests.java
+++ hibernate-java-1/src/test/java/io/hasura/hibernatejava1/HibernateTests.java
@@ -19,12 +19,18 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.Subgraph;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -38,12 +44,14 @@ class HibernateTests {
     }
 
 
-    private EntityManagerFactory sessionFactory;
+    private EntityManagerFactory emFactory;
+
+    private SessionFactory sessionFactory;
     
     @BeforeEach
     protected void setUp() throws Exception {
 	try {
-	    sessionFactory = Persistence.createEntityManagerFactory("example");
+	    emFactory = Persistence.createEntityManagerFactory("example");
 	}
 	catch (Exception e) {
 	    e.printStackTrace(System.err);
@@ -56,35 +64,52 @@ class HibernateTests {
     void save_my_first_object_to_the_db () {
 	Account account = new Account();
 	account.name = "Lisa";
-	EntityManager em = sessionFactory.createEntityManager();
+	EntityManager em = emFactory.createEntityManager();
 	em.persist(account);
     }
 
     @Test
     @Disabled
     public void hql_fetch_accounts () {
-	EntityManager em = sessionFactory.createEntityManager();
+	EntityManager em = emFactory.createEntityManager();
 	em.createQuery("select u from Account u", Account.class).setMaxResults(5).getResultList().forEach(System.out::println);
     }
 
     @Test
     @Disabled
     public void hql_fetch_orders () {
-	EntityManager em = sessionFactory.createEntityManager();
+	EntityManager em = emFactory.createEntityManager();
 	em.createQuery("select u from Order u", Order.class).setMaxResults(5).getResultList().forEach(System.out::println);
     }
 
     @Test
     @Disabled
     public void entitygraph_test () {
-	EntityManager em = sessionFactory.createEntityManager();
+	EntityManager em = emFactory.createEntityManager();
 	EntityGraph<Account> graph = em.createEntityGraph(Account.class);
 	Subgraph<Order> orderSubgraph = graph.addSubgraph("orders");
 	Subgraph<OrderDetail> orderSubgraph2 = orderSubgraph.addSubgraph("orderDetails");
 	orderSubgraph2.addSubgraph("product");
 	Map<String, Object> hints = new HashMap<>();
 	hints.put("javax.persistence.fetchgraph", graph);
-	System.out.println(em.find(Account.class, UUID.fromString("8522dd95-82ba-40f4-914c-e53de362ebcb"), hints));
+	System.out.println(em.find(Account.class, UUID.fromString("d087c9af-b6c9-43a4-83f9-35ad6899b8dd"), hints));
+    }
+
+    @Test
+    public void sessiongraph_test () {
+	var em = emFactory.createEntityManager();
+	var session = em.unwrap(Session.class);
+	var graph = session.createEntityGraph(Account.class);
+	var orderSubgraph = graph.addSubgraph("orders");
+	var orderSubgraph2 = orderSubgraph.addSubgraph("orderDetails");
+	orderSubgraph2.addSubgraph("product");
+	var hints = new HashMap<>();
+	hints.put("javax.persistence.fetchgraph", graph);
+	var accounts = session.byMultipleIds(Account.class)
+	    .withFetchGraph(graph)
+	    .multiLoad(UUID.fromString("d087c9af-b6c9-43a4-83f9-35ad6899b8dd"),
+		       UUID.fromString("d087c9af-b6c9-43a4-83f9-35ad6899b8dd"));
+	accounts.forEach(System.out::println);
     }
 
     @Test
@@ -104,6 +129,7 @@ class HibernateTests {
     }
 
     @Test
+    @Disabled
     void graphql_visit () {
 	Parser parser = new Parser();
 	Document document = parser.parseDocument("query{products(limit:10){id order_details{id order_id order{id account{id}}}}}");
