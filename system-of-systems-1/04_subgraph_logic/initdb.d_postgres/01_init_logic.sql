9af7078504d6e7f9f639739bad6ac944dbf2f66a
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-04-04 14:15:52 -0700

    Made the table table-valued

--- system-of-systems-1/04_subgraph_logic/initdb.d_postgres/01_init_logic.sql
+++ system-of-systems-1/04_subgraph_logic/initdb.d_postgres/01_init_logic.sql
@@ -3,31 +3,29 @@
 create extension http;
 
 create or replace function is_approved (design_id int)
-  returns boolean
+  returns table (design_id int, is_approved boolean)
   language sql
   immutable
   strict
 as $sql$
   select
-  exists (
+  (content->'design_id')::int design_id,
+  (content->'approved')::boolean approved
+  from (
     select
-      *
+      jsonb_array_elements(content) content
       from (
 	select
-	  jsonb_array_elements(content) content
-	  from (
-	    select
-	      content::jsonb->'data'->'product'->'approvals' as content
-	      from
-		http((
-		  'POST',
-		  current_setting('request.headers', true)::json->>'hasura-graphql-endpoint',
-		  array[http_header('x-hasura-admin-secret', current_setting('request.headers', true)::json->>'x-hasura-admin-secret')],
-		  'application/json',
-		  format(
-		    $json$
-		    {"query":"query{product{approvals(where:{design_id:{_in:[%s]}}){design_id approved}}}","variables":{}}	     
-		    $json$, design_id)
-		)::http_request)) foo)
-     where (content->'approved')::boolean)
+	  content::jsonb->'data'->'product'->'approvals' as content
+	  from
+	    http((
+	      'POST',
+	      current_setting('request.headers', true)::json->>'hasura-graphql-endpoint',
+	      array[http_header('x-hasura-admin-secret', current_setting('request.headers', true)::json->>'x-hasura-admin-secret')],
+	      'application/json',
+	      format(
+		$json$
+		{"query":"query{product{approvals(where:{design_id:{_in:[%s]}}){design_id approved}}}","variables":{}}	     
+		$json$, design_id)
+	    )::http_request)) foo)
   $sql$;
