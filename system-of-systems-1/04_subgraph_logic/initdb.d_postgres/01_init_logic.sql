27ef25df832599dd9c94290e5ebabed01ce56171
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-04-04 15:36:10 -0700

    Killing the actions for now.

--- system-of-systems-1/04_subgraph_logic/initdb.d_postgres/01_init_logic.sql
+++ system-of-systems-1/04_subgraph_logic/initdb.d_postgres/01_init_logic.sql
@@ -1,5 +1,8 @@
 -- -*- sql-product: postgres; -*-
 
+create aggregate mul (int) (sfunc = int4mul, stype=int);
+comment on aggregate mul (int) is 'logic to resolve acceptance tests';
+
 create extension http;
 
 create or replace function is_approved (design_id int)
@@ -9,23 +12,51 @@ create or replace function is_approved (design_id int)
   strict
 as $sql$
   select
-  (content->'design_id')::int design_id,
-  (content->'approved')::boolean approved
+  design_id,
+  mul(approved::int)::boolean approved
   from (
     select
-      jsonb_array_elements(content) content
+      (content->'id')::int design_id,
+      (content->'accepted')::boolean approved
       from (
 	select
-	  content::jsonb->'data'->'product'->'approvals' as content
-	  from
-	    http((
-	      'POST',
-	      current_setting('request.headers', true)::json->>'hasura-graphql-endpoint',
-	      array[http_header('x-hasura-admin-secret', current_setting('request.headers', true)::json->>'x-hasura-admin-secret')],
-	      'application/json',
-	      format(
-		$json$
-		{"query":"query{product{approvals(where:{design_id:{_in:[%s]}}){design_id approved}}}","variables":{}}	     
-		$json$, design_id)
-	    )::http_request)) foo)
+	  jsonb_array_elements(content) content
+	  from (
+	    select
+	      content::jsonb->'data'->'engineering'->'design' as content
+	      from
+		http((
+		  'POST',
+		  current_setting('request.headers', true)::json->>'hasura-graphql-endpoint',
+		  array[http_header('x-hasura-admin-secret', current_setting('request.headers', true)::json->>'x-hasura-admin-secret')],
+		  'application/json',
+		  format(
+		    $json$
+		    {"query":"query{engineering{design(where:{id:{_in:[%s]}}){id accepted}}}","variables":{}}		
+		    $json$, design_id)
+		)::http_request)) foo)
+     union
+    select
+      (content->'design_id')::int design_id,
+      (content->'approved')::boolean approved
+      from (
+	select
+	  jsonb_array_elements(content) content
+	  from (
+	    select
+	      content::jsonb->'data'->'product'->'approvals' as content
+	      from
+		http((
+		  'POST',
+		  current_setting('request.headers', true)::json->>'hasura-graphql-endpoint',
+		  array[http_header('x-hasura-admin-secret', current_setting('request.headers', true)::json->>'x-hasura-admin-secret')],
+		  'application/json',
+		  format(
+		    $json$
+		    {"query":"query{product{approvals(where:{design_id:{_in:[%s]}}){design_id approved}}}","variables":{}}	     
+		    $json$, design_id)
+		)::http_request)) foo)) bar
+  group by design_id;
   $sql$;
+
+  
