d53c862abce11cba6451cb9eab27c497d063ebff
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-05-31 08:39:00 -0700

    Added Action parameters and enhanced the Kriti templates

--- hasura-and-openapi/initdb.d-postgres/03_add_functions.sql
+++ hasura-and-openapi/initdb.d-postgres/03_add_functions.sql
@@ -1,12 +1,28 @@
 -- -*- sql-product: postgres; -*-
 
-create or replace function authorization_accounts_v3 ()
-  returns table (
-    id uuid)
+create or replace function authorization_accounts_v3 (shard int)
+  returns table(id uuid)
   language sql
-  stable
-  leakproof
-  parallel safe
-as $$
-  select id from account limit 3;
-$$;
+  stable parallel safe leakproof
+as $function$
+  select
+  id
+  from
+  (
+    select
+      id,
+      row_number() over (order by name) rownum
+      from
+	account) foo
+  where
+  rownum % shard = 0;
+  $function$;
+
+create or replace function authorization_accounts_v3_scalar (shard int)
+  returns uuid[]
+  language sql
+  stable parallel safe leakproof
+as $function$
+  select array_agg(id) as ids from authorization_accounts_v3(shard)
+  $function$;
+
