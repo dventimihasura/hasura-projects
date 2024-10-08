3e650d6aeb468849235c1331e56167a6123ded3d
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-10-08 15:45:00 -0700

    Moved the data into a seed

--- custom-event-triggers-with-many-related-tables-1/migrations/default/1728416848603_seed/up.sql
+++ custom-event-triggers-with-many-related-tables-1/migrations/default/1728416848603_seed/up.sql
@@ -5,17 +5,6 @@
 -- Dumped from database version 16.4 (Debian 16.4-1.pgdg120+2)
 -- Dumped by pg_dump version 16.3 (Ubuntu 16.3-1.pgdg22.04+1)
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- Data for Name: Artist; Type: TABLE DATA; Schema: public; Owner: postgres
 --
