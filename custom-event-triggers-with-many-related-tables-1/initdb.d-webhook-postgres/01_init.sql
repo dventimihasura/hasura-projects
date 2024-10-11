530d337329d217ea7c8f00dda103ea10e420f345
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-10-11 14:27:24 -0700

    Added a new function

--- custom-event-triggers-with-many-related-tables-1/initdb.d-webhook-postgres/01_init.sql
+++ custom-event-triggers-with-many-related-tables-1/initdb.d-webhook-postgres/01_init.sql
@@ -13,9 +13,24 @@ create or replace function insert_albumsummary_one ("albumId" int, "payload" jso
 	{"query":"mutation MyMutation($albumId:Int,$payload:jsonb){insert_AlbumSummary_one(object:{AlbumId:$albumId,Payload:$payload}){AlbumId Payload}}","variables":{"albumId":%s,"payload":%s}}
 	$$,
 	$1,
-	$2
-    ),
-    'application/json'
-  )
+	$2),
+    'application/json')
   $function$;
 
+create or replace function insert_albumsummary_one ("albumId" int) returns text language sql as $function$
+  with
+  summary as (
+    select
+      content
+      from
+	http_post(
+	  'http://graphql-engine:8080/v1/graphql',
+	  format(
+	    $${"query":"query MyQuery{Album_by_pk(AlbumId:%s){AlbumId ArtistId Title Tracks{AlbumId Bytes Composer GenreId MediaTypeId Milliseconds Name UnitPrice MediaType{MediaTypeId Name}Genre{GenreId Name}}}}","variables":{}}$$,
+	    $1),
+	    'application/json'))
+  select
+  insert_albumsummary_one($1, content::jsonb)
+  from
+  summary;
+  $function$;
